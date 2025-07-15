641936b8ce89bb5d3fad3d15bed37084a5ee56ce
taxiotti <lucas.taxiotti@germinare.org.br>
2025-07-15 19:05:53 -0300

    [CHORE] define app color themes

--- app/src/main/java/com/germinare/simbia_mobile/MainActivity.java
+++ app/src/main/java/com/germinare/simbia_mobile/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -23,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
