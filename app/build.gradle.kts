2291c6ac28abbfd14ee5130eecb3a6683484a202
Pedro Gabriel <pedro.correia@germinare.org.br>
2025-08-28 09:13:38 -0300

    feat: create home screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.firebase.firestore)
     implementation(libs.activity)
     implementation(libs.recyclerview)
+    implementation(libs.legacy.support.v4)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
