36fe4c319413c88b5de92415746542e7a8380f15
Pedro Gabriel <pedro.correia@germinare.org.br>
2025-09-06 20:40:59 -0300

    feat: add chatbot messages screen

---
+++ app/src/main/java/com/germinare/simbia_mobile/ui/features/home/fragments/eva/EvaMessagesFragment.java
@@ -0,0 +1,101 @@
+package com.germinare.simbia_mobile.ui.features.home.fragments.eva;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.germinare.simbia_mobile.databinding.FragmentEvaMessagesBinding;
+import com.germinare.simbia_mobile.ui.features.home.fragments.eva.adapter.MessageChatBot;
+import com.germinare.simbia_mobile.ui.features.home.fragments.eva.adapter.MessagesChatBotAdapter;
+
+import java.util.UUID;
+
+/**
+ * A fragment representing a list of Items.
+ */
+public class EvaMessagesFragment extends Fragment {
+
+    private FragmentEvaMessagesBinding binding;
+    private MessagesChatBotAdapter adapter;
+    private static final String ARG_COLUMN_COUNT = "column-count";
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public EvaMessagesFragment() {
+    }
+
+    @SuppressWarnings("unused")
+    public static EvaMessagesFragment newInstance(int columnCount) {
+        EvaMessagesFragment fragment = new EvaMessagesFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_COLUMN_COUNT, columnCount);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentEvaMessagesBinding.inflate(inflater, container, false);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Bundle envelope = getArguments();
+        binding.listMessagesEva.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        adapter = new MessagesChatBotAdapter(getContext());
+        binding.listMessagesEva.setAdapter(adapter);
+
+        if (envelope != null){
+            adapter.addMessage(new MessageChatBot(
+                    UUID.randomUUID().toString(),
+                    "1",
+                    "2",
+                    envelope.getString("message")
+            ));
+            adapter.addMessage(new MessageChatBot(
+                    UUID.randomUUID().toString(),
+                    "2",
+                    "1",
+                    envelope.getString("message")
+            ));
+        }
+
+        binding.imageView6.setOnClickListener(V -> {
+            final String messageContent = binding.etChatMessage.getText().toString();
+
+            if (!messageContent.isEmpty()){
+                adapter.addMessage(new MessageChatBot(
+                        UUID.randomUUID().toString(),
+                        "1",
+                        "2",
+                        messageContent
+                ));
+                adapter.addMessage(new MessageChatBot(
+                        UUID.randomUUID().toString(),
+                        "2",
+                        "1",
+                        messageContent
+                ));
+            }
+
+        });
+
+    }
+}
---
+++ app/src/main/java/com/germinare/simbia_mobile/ui/features/home/fragments/eva/adapter/MessagesChatBotAdapter.java
@@ -0,0 +1,88 @@
+package com.germinare.simbia_mobile.ui.features.home.fragments.eva.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.germinare.simbia_mobile.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link MessageChatBot}.
+ */
+public class MessagesChatBotAdapter extends RecyclerView.Adapter<MessagesChatBotAdapter.MessagesViewHolder> {
+
+    private static final int MESSAGE_TYPE_SEND = 1;
+    private static final int MESSAGE_TYPE_RECEIVED = 2;
+    private Context context;
+    private List<MessageChatBot> messages;
+
+    public MessagesChatBotAdapter(Context context) {
+        this.context = context;
+        this.messages = new ArrayList<>();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return messages.get(position).getIdUserSent().equals("1") ? MESSAGE_TYPE_SEND : MESSAGE_TYPE_RECEIVED;
+    }
+
+    @Override
+    public MessagesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == MESSAGE_TYPE_SEND) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(
+                    R.layout.messages_row_sent,
+                    parent,
+                    false
+            );
+            return new MessagesViewHolder(view);
+        } else {
+            View view = LayoutInflater.from(parent.getContext()).inflate(
+                    R.layout.messages_row_received,
+                    parent,
+                    false
+            );
+            return new MessagesViewHolder(view);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(final MessagesViewHolder holder, int position) {
+        final MessageChatBot message = messages.get(position);
+
+        if (message.getIdUserSent().equals("1")){
+            holder.txMessageSent.setText(message.getContent());
+        }else{
+            holder.txMessageReceived.setText(message.getContent());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return messages.size();
+    }
+
+    public List<MessageChatBot> getMessages() {
+        return this.messages;
+    }
+
+    public void addMessage(MessageChatBot message){
+        this.messages.add(message);
+    }
+
+    static class MessagesViewHolder extends RecyclerView.ViewHolder {
+        final TextView txMessageSent, txMessageReceived;
+
+        MessagesViewHolder(View view){
+            super(view);
+            this.txMessageSent = view.findViewById(R.id.txMessageSent);
+            this.txMessageReceived = view.findViewById(R.id.txMessageReceived);
+        }
+    }
+}
